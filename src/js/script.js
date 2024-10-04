a7857d72e271eedcfd4fa495ee41f79af16c20a5
chugani05 <chugani205@gmail.com>
2024-10-04 16:01:09 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -21,15 +21,15 @@ button.addEventListener("click", () => {
 async function getLegend() {
     const url = "https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json"; // API URL with champion data
     const response = await fetch(url); // Send a request to the API
-    console.log(response);
     const data = (await response.json()).data; // Parse the response to JSON and access the "data" property that contains the champions
-
+    
     // Loop through each champion (legend) in the data obtained from the API
     for (let legend in data) {
         // Create a new instance of the Legend class for each champion and add it to the legends array
         console.log(legend);
         legends.push(new Legend(data[legend]));
     }
+    console.log(legends);
 
     // Remove the "Loading data..." message from the DOM once the data is loaded
     loadingDataText.remove();
